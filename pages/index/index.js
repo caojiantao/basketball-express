63e96e01b4c80ecbd73b1a305beff18a6e2ae9a4
caojiantao <caojiantao@zhuanzhuan.com>
2023-12-29 12:02:06 +0800

    完善展示样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -26,6 +26,7 @@ Page({
         console.log(nextData);
         let gameList = nextData.props.pageProps.gameList;
         let date = new Date();
+        // let day = '20231228';
         let day = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
         let todayGame = null;
         for (let game of gameList) {
@@ -47,6 +48,25 @@ Page({
   onClickMatch(e) {
     console.log(e);
     let matchId = e.currentTarget.dataset.id;
+
+    let match = null;
+    for (let item of this.data.todayGame.matchList) {
+        if (item.matchId == matchId) {
+            match = item;
+            break;
+        }
+    }
+    console.log('点击的比赛信息', match);
+    if (match.matchStatus != 'COMPLETED') {
+        wx.showToast({
+          icon: 'error',
+          title: '未完赛',
+          mask: true,
+          duration: 1000,
+        })
+        return;
+    }
+
     let requestTask = wx.request({
       url: `https://m.hupu.com/nba/live/${matchId}`,
       redirect: 'manual',
--- pages/match-detail/match-detail.js
+++ pages/match-detail/match-detail.js
@@ -80,5 +80,5 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
 })
